9268649161d5e7d9876b5b5465bae0d07b5a099e
Kaloyan Petrov <k.petrov1206@gmail.com>
2021-12-20 15:12:06 +0200

    Added login functionality and sidebar

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Sidebar from "../components/sidebar";
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main>
+        <Sidebar />
+        {/* Main section */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+
+    </div>
+  )
+}
